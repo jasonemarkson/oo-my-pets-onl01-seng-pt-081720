24fd8725cc589167353665eaa3592657fc156c50
Jason Markson <jasonmarkson76@gmail.com>
2020-09-16 18:36:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -53,7 +53,7 @@ class Owner
   
   def sell_pets
     self.dogs.each { |dog| dog.mood = "nervous" } && self.cats.each { |cat| cat.mood = "nervous" }
-    self.dogs.each { |dog| dog.mood = "nervous" } && self.cats.each { |cat| cat.mood = "nervous" }
+    self.dogs.each { |dog| dog.owner = nil } && self.cats.each { |cat| cat.owner = nil }
 
   end
 end
